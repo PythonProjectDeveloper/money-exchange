2cce4bb357b6afd5458453e0c3292520b345d7bd
PythonProjectDeveloper <SleepyDevil@protonmail.com>
2019-02-28 10:09:24 +0300

    rewrite function implementation

--- src/index.js
+++ src/index.js
@@ -4,21 +4,17 @@ module.exports = function makeExchange(currency) {
     // Return an object containing the minimum number of coins needed to make change
     if ( currency > 10000 ) return {error: "You are rich, my friend! We don't have so much coins for exchange"}
     
-    let COINS = [ {name: 'H', value: 50}, {name: 'Q', value: 25}, {name: 'D', value: 10}, {name: 'N', value: 5}, {name: 'P', value:1} ]
+    const coins = [ {name: 'H', value: 50}, {name: 'Q', value: 25}, {name: 'D', value: 10}, {name: 'N', value: 5}, {name: 'P', value:1} ]
     let result = {}
-    let i = 0
 
-    while ( currency > 0 ) {
-        let coin = COINS[i]
-
-        if ( currency - coin.value < 0 ) {
-            i += 1
-            continue
-        }
+    coins.forEach( coin => {
+      let count = parseInt( currency / coin.value )
+      if ( count > 0 ) {
+        result[coin.name] = count
+        currency %= coin.value
+      }
         
-        result[coin.name] = (coin.name in result) ? result[coin.name] + 1 : 1
+    })
 
-	currency -= coin.value
-    }
     return result
 }
